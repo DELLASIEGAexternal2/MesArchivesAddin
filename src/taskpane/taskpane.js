30113e975b595d2228974b37fa29f1794950bb6b
DELLASIEGAexternal2 <260647862+DELLASIEGAexternal2@users.noreply.github.com>
2026-03-09 17:21:51 +0100

    Add files via upload

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,23 @@
+Office.onReady(() => {
+
+console.log("Mes Archives chargé");
+
+});
+
+function openArchive(year){
+
+alert("Ouverture archives " + year);
+
+}
+
+function modop(){
+
+window.open("https://intranet/modop");
+
+}
+
+function about(){
+
+alert("Mes Archives v1");
+
+}
