039fe0f79b28482052b6d487c667ec379ca46982
DELLASIEGAexternal2 <260647862+DELLASIEGAexternal2@users.noreply.github.com>
2026-03-10 12:27:31 +0100

    Add async function to fetch user data from Graph API

--- graph/graphService.js
+++ graph/graphService.js
@@ -1,1 +1,22 @@
 
+async function getUser(){
+
+const token = await OfficeRuntime.auth.getAccessToken();
+
+const response = await fetch(
+
+"https://graph.microsoft.com/v1.0/me",
+
+{
+
+headers:{
+
+"Authorization":"Bearer " + token
+
+}
+
+});
+
+return await response.json();
+
+}
