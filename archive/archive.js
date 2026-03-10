f41a5d1270362ad12b9498221bd64bd9377a7e99
DELLASIEGAexternal2 <260647862+DELLASIEGAexternal2@users.noreply.github.com>
2026-03-10 14:20:30 +0100

    Implement message loading by year in archive.js
    Add functionality to load and display messages for a selected year.

--- archive/archive.js
+++ archive/archive.js
@@ -1,1 +1,34 @@
+Office.onReady(() => {
 
+  const params = new URLSearchParams(window.location.search);
+  const year = params.get("year");
+
+  document.getElementById("title").innerText = "Archives " + year;
+
+  loadMessages(year);
+
+});
+
+async function loadMessages(year){
+
+  const messages = await getMessagesByYear(year);
+
+  const container = document.getElementById("messages");
+
+  messages.forEach(m => {
+
+    const div = document.createElement("div");
+
+    div.className="mail";
+
+    div.innerHTML = `
+      <b>${m.subject}</b><br>
+      ${m.from.emailAddress.address}<br>
+      ${m.receivedDateTime}
+    `;
+
+    container.appendChild(div);
+
+  });
+
+}
