7c62c6adc5fbad324baf0dd46396410f066eaa7b
DELLASIEGAexternal2 <260647862+DELLASIEGAexternal2@users.noreply.github.com>
2026-06-15 08:24:07 +0200

    Ajout architecture hybride Outlook Add-in

--- api/obj/Debug/net8.0/MesArchivesAgent.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/MesArchivesAgent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MesArchivesAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66af20360907d2a92f1acc956970f7215203bee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c75d3e98786eef95b877db1260cfbce80570bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MesArchivesAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MesArchivesAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
